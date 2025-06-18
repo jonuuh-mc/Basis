381f27dc78adf3fc22314b7ecdaa787573770a53
jonuuh <jhammond.123x@gmail.com>
2025-06-18 08:10:06 -0700

    Implementation of GuiElement's section of the new targeted event system - ft. some other little changes:   - making many fields private   - implementing some utility functions like collectMatchingElements and getPropagationPath   - creating bound getters (e.g getRightBound() to be used later for things like the reimplementation of the dreaded scissor testing for scrollable containers)

--- src/main/java/io/jonuuh/core/lib/gui/element/GuiElement.java
+++ src/main/java/io/jonuuh/core/lib/gui/element/GuiElement.java
@@ -4,21 +4,19 @@ import io.jonuuh.core.lib.gui.AbstractGuiScreen;
 import io.jonuuh.core.lib.gui.element.container.GuiContainer;
 import io.jonuuh.core.lib.gui.element.container.GuiRootContainer;
 import io.jonuuh.core.lib.gui.properties.GuiColorType;
-import io.jonuuh.core.lib.gui.properties.GuiEventType;
 import io.jonuuh.core.lib.gui.properties.Spacing;
 import io.jonuuh.core.lib.util.Color;
 import io.jonuuh.core.lib.util.RenderUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.audio.PositionedSoundRecord;
-import net.minecraft.client.audio.SoundHandler;
-import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
-import java.util.function.Function;
+import java.util.function.Predicate;
 
 public abstract class GuiElement
 {
@@ -27,16 +25,11 @@ public abstract class GuiElement
 
     public static final float DEFAULT_WIDTH = 100;
     public static final float DEFAULT_HEIGHT = 20;
-
-    /** A resource pointing to the default mouse down sound */
-    protected static final ResourceLocation resourceClickSound;
     protected static final ResourceLocation resourceGenericBackgroundTex;
 
     static
     {
         mc = Minecraft.getMinecraft();
-        // TODO: implement similar feature to colorMap? propagate up tree until finding a parent that has this defined?
-        resourceClickSound = new ResourceLocation("core:click");
         resourceGenericBackgroundTex = new ResourceLocation("core:textures/background_generic.png");
     }
 
@@ -48,79 +41,33 @@ public abstract class GuiElement
     /** A map of colors that may be used by this element */
     protected Map<GuiColorType, Color> colorMap;
 
-    /**
-     * A map of pre-event behavior lambdas.
-     * <p>
-     * Allows for 'injecting' an arbitrary function call BEFORE this element's default behavior for an event.
-     * This function call should return a boolean: whether it should override the element's default behavior for this event.
-     * <p>
-     * All possible events are normally dispatched by the GuiScreen, EXCEPT FOR 'CUSTOM',
-     * which may be dispatched by the element itself, at a time which is defined by that element.
-     * <p>
-     * e.g. A GuiSingleSlider tries to call any assigned CUSTOM pre event behavior before its value is updated via
-     * other events: KEY_INPUT, MOUSE_SCROLL, SCREEN_DRAW
-     * (TODO: this design is so weird)
-     * <p>
-     * Keys are enums representing different events a GuiElement can act on, and values are functions which will
-     * be executed (if they exist) before their associated event's default behavior.
-     */
-    protected Map<GuiEventType, Function<GuiElement, Boolean>> preEventBehaviors;
-
-    /**
-     * A map of post-event behavior lambdas.
-     * <p>
-     * Allows for 'injecting' an arbitrary function call AFTER this element's default behavior for an event.
-     * <p>
-     * All possible events are normally dispatched by the GuiScreen, EXCEPT FOR 'CUSTOM',
-     * which may be dispatched by the element itself, at a time which is defined by that element.
-     * <p>
-     * e.g. A GuiSingleSlider tries to call any assigned CUSTOM post event behavior after its value is updated via
-     * other events: KEY_INPUT, MOUSE_SCROLL, SCREEN_DRAW
-     * (TODO: this design is so weird)
-     * <p>
-     * Keys are enums representing different events a GuiElement can act on, and values are consumers which will
-     * be executed (if they exist) after their associated event's default behavior.
-     * <p>
-     * Should probably be used way more commonly than pre-event behaviors, e.g. for updating the value(s) of
-     * {@link io.jonuuh.core.lib.config.setting.types.Setting}s if associated with an element,
-     * after that element's state is changed through its default event behavior
-     */
-    protected Map<GuiEventType, Consumer<GuiElement>> postEventBehaviors;
-
     /** Element x position within its parent */
-    protected float localXPos;
+    private float localXPos;
     /** Element y position within its parent */
-    protected float localYPos;
+    private float localYPos;
     /** The sum of all world xPos from this element's ancestors */
-    protected float inheritedXPos;
+    private float inheritedXPos;
     /** The sum of all world yPos from this element's ancestors */
-    protected float inheritedYPos;
+    private float inheritedYPos;
 
     /** Width of this element */
-    protected float width;
+    private float width;
     /** Height of this element */
-    protected float height;
+    private float height;
 
     /** Which symbolic layer this element is on, should be equal to how many parents this element has */
-    protected int zLevel;
+    private int zLevel;
     /** Whether this element is visible (should be drawn to screen) */
-    protected boolean visible;
-    /** Whether this element can be interacted with (usually whether it can be clicked, but could be defined differently in subclasses) */
-    protected boolean enabled;
+    private boolean visible;
     /** Whether this element is currently hovered; Updated constantly via onScreenDraw (not via onScreenTick because mouse pos is needed) */
-    protected boolean hovered;
-    /** Whether the mouse is currently pressed down on this element (!= hovered, is set true on mouse down and false on mouse release */
-    protected boolean mouseDown;
+    private boolean hovered;
+
+    private Spacing margin;
+    private Spacing padding;
+
+    private String tooltipStr;
 
     protected boolean debug;
-
-    protected Spacing margin;
-    protected Spacing padding;
-
-    protected String tooltipStr;
-
-    // TODO: use a ticker with onScreenTick instead for tooltips?
-    protected int hoverTimeCounter;
 
     protected GuiElement(String elementName, float localXPos, float localYPos, float width, float height)
     {
@@ -134,12 +81,8 @@ public abstract class GuiElement
         this.padding = new Spacing(0);
 
         this.visible = true;
-        this.enabled = true;
-
         this.debug = true;
 
-        this.preEventBehaviors = new HashMap<>();
-        this.postEventBehaviors = new HashMap<>();
         this.colorMap = new HashMap<>();
     }
 
@@ -162,6 +105,16 @@ public abstract class GuiElement
         this.parent = parent;
     }
 
+    public boolean hasParent()
+    {
+        return parent != null;
+    }
+
+    public int getNumParents()
+    {
+        return hasParent() ? parent.getNumParents() + 1 : 0;
+    }
+
     /**
      * Propagate up the tree to retrieve a reference to the GuiScreen containing this element held by the GuiRootContainer
      */
@@ -175,16 +128,6 @@ public abstract class GuiElement
         {
             return parent.getGuiScreen();
         }
-    }
-
-    public boolean hasParent()
-    {
-        return parent != null;
-    }
-
-    public int getNumParents()
-    {
-        return hasParent() ? parent.getNumParents() + 1 : 0;
     }
 
     public GuiElement putColor(GuiColorType type, Color color)
@@ -328,29 +271,9 @@ public abstract class GuiElement
         this.tooltipStr = tooltipStr;
     }
 
-    public boolean isEnabled()
-    {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled)
-    {
-        this.enabled = enabled;
-    }
-
     public boolean isFocused()
     {
         return getGuiScreen().getCurrentFocus() == this;
-    }
-
-    public boolean isMouseDown()
-    {
-        return mouseDown;
-    }
-
-    public void setMouseDown(boolean mouseDown)
-    {
-        this.mouseDown = mouseDown;
     }
 
     public Spacing getMargin()
@@ -375,20 +298,74 @@ public abstract class GuiElement
 
     public boolean isPointWithinBounds(float x, float y)
     {
-        return (x >= worldXPos()) && (x < worldXPos() + getWidth())
-                && (y >= worldYPos()) && (y < worldYPos() + getHeight());
+        return (x > getLeftBound()) && (x < getRightBound())
+                && (y > getTopBound()) && (y < getBottomBound());
     }
 
-    public GuiElement getGreatestZLevelHovered(GuiElement currGreatest)
+    public float getLeftBound()
     {
-        if (hovered && enabled && visible)
+        return worldXPos();
+    }
+
+    public float getRightBound()
+    {
+        return worldXPos() + getWidth();
+    }
+
+    public float getTopBound()
+    {
+        return worldYPos();
+    }
+
+    public float getBottomBound()
+    {
+        return worldYPos() + getHeight();
+    }
+
+    public void onScreenDraw(int mouseX, int mouseY, float partialTicks)
+    {
+        if (!isVisible())
         {
-            if (this.zLevel > currGreatest.zLevel)
-            {
-                return this;
-            }
+            return;
         }
-        return currGreatest;
+
+        setHovered(isPointWithinBounds(mouseX, mouseY));
+
+//            if (!enabled)
+//            {
+//                RenderUtils.drawRectangle(worldXPos(), worldYPos(), getWidth(), getHeight(), new Color("#aa181818"));
+//            }
+
+        if (debug)
+        {
+            GL11.glEnable(GL11.GL_LINE_SMOOTH);
+            GL11.glLineWidth(0.8F);
+            RenderUtils.drawRectangle(GL11.GL_LINE_LOOP, worldXPos(), worldYPos(), getWidth(), getHeight(),
+                    isFocused() ? new Color("#00ff00") : new Color("#ff55ff"));
+            GL11.glLineWidth(1F);
+            GL11.glDisable(GL11.GL_LINE_SMOOTH);
+
+            mc.fontRendererObj.drawString(String.valueOf(getZLevel()), getRightBound() - mc.fontRendererObj.getStringWidth(String.valueOf(getZLevel())),
+                    getBottomBound() - mc.fontRendererObj.FONT_HEIGHT, getColor(GuiColorType.ACCENT2).toPackedARGB(), true);
+        }
+    }
+
+    /**
+     * Get an event propagation path from the root of the element tree to this element.
+     *
+     * @return The path; A list of all this element's ancestors
+     */
+    public List<GuiElement> getPropagationPath()
+    {
+        List<GuiElement> path = new ArrayList<>();
+
+        GuiElement element = this;
+        while (element != null)
+        {
+            path.add(0, element); // Insert at beginning to reverse order
+            element = element.parent;
+        }
+        return path;
     }
 
     /**
@@ -401,264 +378,12 @@ public abstract class GuiElement
         action.accept(this);
     }
 
-    /**
-     * Assign some pre-event behavior to this element
-     * <p>
-     * The behavior should return a boolean where true means it should override the default behavior, false means it should not
-     *
-     * @param eventType An enum representing the event this behavior should be associated with
-     * @param behavior A pre-event behavior
-     * @see GuiElement#preEventBehaviors
-     */
-    public void assignPreEventBehavior(GuiEventType eventType, Function<GuiElement, Boolean> behavior)
+    public void collectMatchingElements(List<GuiElement> collector, Predicate<GuiElement> predicate)
     {
-        preEventBehaviors.put(eventType, behavior);
-    }
-
-    /**
-     * Assign some post-event behavior to this element
-     *
-     * @param eventType An enum representing the event this behavior should be associated with
-     * @param behavior A post-event behavior
-     * @see GuiElement#postEventBehaviors
-     */
-    public void assignPostEventBehavior(GuiEventType eventType, Consumer<GuiElement> behavior)
-    {
-        postEventBehaviors.put(eventType, behavior);
-    }
-
-    /**
-     * If it exists, apply the pre-event behavior to this element for some event type
-     *
-     * @param eventType An enum representing the event this behavior should be associated with
-     * @return true if this behavior exists and should override the default behavior for this event, otherwise false
-     */
-    protected boolean tryApplyPreEventBehavior(GuiEventType eventType)
-    {
-        Function<GuiElement, Boolean> behavior = preEventBehaviors.get(eventType);
-        return behavior != null && behavior.apply(this);
-    }
-
-    /**
-     * If it exists, apply the post-event behavior to this element for some event type
-     *
-     * @param eventType An enum representing the event this behavior should be associated with
-     */
-    protected void tryApplyPostEventBehavior(GuiEventType eventType)
-    {
-        Consumer<GuiElement> behavior = postEventBehaviors.get(eventType);
-        if (behavior != null)
+        if (predicate.test(this))
         {
-            behavior.accept(this);
+            collector.add(this);
         }
-    }
-
-    public final void handleInitGuiEvent(ScaledResolution scaledResolution)
-    {
-        boolean doDefaultBehavior = !tryApplyPreEventBehavior(GuiEventType.INIT);
-
-        if (doDefaultBehavior)
-        {
-        }
-
-        onInitGui(scaledResolution);
-
-        tryApplyPostEventBehavior(GuiEventType.INIT);
-    }
-
-    protected void onInitGui(ScaledResolution scaledResolution)
-    {
-    }
-
-    public final void handleCloseGuiEvent()
-    {
-        boolean doDefaultBehavior = !tryApplyPreEventBehavior(GuiEventType.CLOSE);
-
-        if (doDefaultBehavior)
-        {
-        }
-
-        onCloseGui();
-
-        tryApplyPostEventBehavior(GuiEventType.CLOSE);
-    }
-
-    protected void onCloseGui()
-    {
-    }
-
-    public final void handleScreenDrawEvent(int mouseX, int mouseY, float partialTicks)
-    {
-        boolean doDefaultBehavior = !tryApplyPreEventBehavior(GuiEventType.SCREEN_DRAW);
-
-        if (doDefaultBehavior)
-        {
-            if (visible)
-            {
-                hovered = isPointWithinBounds(mouseX, mouseY);
-
-                onScreenDraw(mouseX, mouseY, partialTicks);
-
-                if (!enabled)
-                {
-                    RenderUtils.drawRectangle(worldXPos(), worldYPos(), getWidth(), getHeight(), new Color("#aa181818"));
-                }
-
-                if (debug)
-                {
-                    GL11.glEnable(GL11.GL_LINE_SMOOTH);
-                    GL11.glLineWidth(0.8F);
-                    RenderUtils.drawRectangle(GL11.GL_LINE_LOOP, worldXPos(), worldYPos(), getWidth(), getHeight(),
-                            isFocused() ? new Color("#00ff00") : new Color("#ff55ff"));
-                    GL11.glLineWidth(1F);
-                    GL11.glDisable(GL11.GL_LINE_SMOOTH);
-
-                    mc.fontRendererObj.drawString(String.valueOf(zLevel), worldXPos() + getWidth() - mc.fontRendererObj.getStringWidth(String.valueOf(zLevel)),
-                            worldYPos() + getHeight() - mc.fontRendererObj.FONT_HEIGHT, getColor(GuiColorType.ACCENT2).toPackedARGB(), true);
-                }
-            }
-            return;
-        }
-
-        onScreenDraw(mouseX, mouseY, partialTicks);
-
-        tryApplyPostEventBehavior(GuiEventType.SCREEN_DRAW);
-    }
-
-    protected abstract void onScreenDraw(int mouseX, int mouseY, float partialTicks);
-
-    public final void handleScreenTickEvent()
-    {
-        boolean doDefaultBehavior = !tryApplyPreEventBehavior(GuiEventType.SCREEN_TICK);
-
-        if (doDefaultBehavior)
-        {
-            // TODO: something like this?
-            if (hovered && hoverTimeCounter != 20)
-            {
-                hoverTimeCounter = 20;
-            }
-            else if (hoverTimeCounter > 0)
-            {
-                hoverTimeCounter--;
-            }
-        }
-
-        onScreenTick();
-
-        tryApplyPostEventBehavior(GuiEventType.SCREEN_TICK);
-    }
-
-    protected void onScreenTick()
-    {
-    }
-
-    public final void handleMouseDownEvent(int mouseX, int mouseY)
-    {
-        boolean doDefaultBehavior = !tryApplyPreEventBehavior(GuiEventType.MOUSE_DOWN);
-
-        if (doDefaultBehavior)
-        {
-            mouseDown = true;
-            playClickSound(mc.getSoundHandler());
-        }
-
-        onMouseDown(mouseX, mouseY);
-
-        tryApplyPostEventBehavior(GuiEventType.MOUSE_DOWN);
-    }
-
-    protected void onMouseDown(int mouseX, int mouseY)
-    {
-    }
-
-    public final void handleMouseUpEvent(int mouseX, int mouseY)
-    {
-        boolean doDefaultBehavior = !tryApplyPreEventBehavior(GuiEventType.MOUSE_UP);
-
-        if (doDefaultBehavior)
-        {
-            mouseDown = false;
-        }
-
-        onMouseUp(mouseX, mouseY);
-
-        tryApplyPostEventBehavior(GuiEventType.MOUSE_UP);
-    }
-
-    protected void onMouseUp(int mouseX, int mouseY)
-    {
-    }
-
-    public final void handleMouseScrollEvent(int wheelDelta)
-    {
-        boolean doDefaultBehavior = !tryApplyPreEventBehavior(GuiEventType.MOUSE_SCROLL);
-
-        if (doDefaultBehavior)
-        {
-        }
-
-        onMouseScroll(wheelDelta);
-
-        tryApplyPostEventBehavior(GuiEventType.MOUSE_SCROLL);
-    }
-
-    protected void onMouseScroll(int wheelDelta)
-    {
-    }
-
-    public final void handleMouseDragEvent(int mouseX, int mouseY, int clickedMouseButton, long msHeld)
-    {
-        boolean doDefaultBehavior = tryApplyPreEventBehavior(GuiEventType.MOUSE_DRAG);
-
-        if (doDefaultBehavior)
-        {
-            // note -> mouseDown should only be true for 1 element at a time: the highest z level that was hovered during click event
-            if (!mouseDown)
-            {
-                return;
-            }
-        }
-
-        onMouseDrag(mouseX, mouseY, clickedMouseButton, msHeld);
-
-        tryApplyPostEventBehavior(GuiEventType.MOUSE_DRAG);
-    }
-
-    protected void onMouseDrag(int mouseX, int mouseY, int clickedMouseButton, long msHeld)
-    {
-    }
-
-    public final void handleKeyInputEvent(char typedChar, int keyCode)
-    {
-        boolean doDefaultBehavior = tryApplyPreEventBehavior(GuiEventType.KEY_INPUT);
-
-        if (doDefaultBehavior)
-        {
-        }
-
-        onKeyTyped(typedChar, keyCode);
-
-        tryApplyPostEventBehavior(GuiEventType.KEY_INPUT);
-    }
-
-    protected void onKeyTyped(char typedChar, int keyCode)
-    {
-    }
-
-    protected boolean handlePreCustomEvent()
-    {
-        return !tryApplyPreEventBehavior(GuiEventType.CUSTOM);
-    }
-
-    protected void handlePostCustomEvent()
-    {
-        tryApplyPostEventBehavior(GuiEventType.CUSTOM);
-    }
-
-    protected void playClickSound(SoundHandler soundHandler)
-    {
-        soundHandler.playSound(PositionedSoundRecord.create(resourceClickSound, 2.0F));
     }
 
     @Override
