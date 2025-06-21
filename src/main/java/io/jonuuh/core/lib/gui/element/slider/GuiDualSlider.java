36e815a9c714d021fe12372e3408e9b5748714a1
jonuuh <jhammond.123x@gmail.com>
2025-06-21 16:53:50 -0700

    10th iteration of slider elements I'm so tired of trying to make sliders perfect holy fuck And they still have so many problems Oh my god

--- src/main/java/io/jonuuh/core/lib/gui/element/slider/GuiDualSlider.java
+++ src/main/java/io/jonuuh/core/lib/gui/element/slider/GuiDualSlider.java
@@ -3,6 +3,7 @@ package io.jonuuh.core.lib.gui.element.slider;
 import io.jonuuh.core.lib.gui.event.input.MouseDownEvent;
 import io.jonuuh.core.lib.gui.event.input.MouseScrollEvent;
 import io.jonuuh.core.lib.gui.properties.GuiColorType;
+import io.jonuuh.core.lib.util.Color;
 import io.jonuuh.core.lib.util.MathUtils;
 import io.jonuuh.core.lib.util.RenderUtils;
 import net.minecraft.util.ResourceLocation;
@@ -11,78 +12,68 @@ import org.lwjgl.opengl.GL11;
 public class GuiDualSlider extends GuiSlider
 {
     protected static final ResourceLocation pointerResource = new ResourceLocation("core:textures/pointer.png");
-    protected float normalValueRight;
-    protected boolean isLastHeldPointerLeft;
+    protected float normalValueEnd;
+    /**
+     * MouseDown and MouseScroll events will update the normal value corresponding to this pointer.
+     * <p>
+     * (Pointer.START={@link GuiSlider#normalValue}, Pointer.END={@link GuiDualSlider#normalValueEnd})
+     */
+    protected Pointer lastHeldPointer;
+    /**
+     * Used for tracking which of the two pointers should currently be 'highlighted' through scaling in drawPointer().
+     * <p>
+     * Will also get cached in {@link GuiDualSlider#lastHeldPointer} on MouseDown events.
+     */
+    protected Pointer closestPointerToMouse;
 
     public GuiDualSlider(Builder builder)
     {
         super(builder);
-        setRightValue(builder.startValueRight);
+        // This is the same as `setValue(builder.endPointerValue, Pointer.END);`, but calling setValue() from constructor
+        // is a bad idea as it also tries to apply the event behavior in setNormalizedValue()
+        double value = MathUtils.normalize(builder.endPointerValue, min, max);
+        float clampAgainstStart = (float) MathUtils.clamp(value, getNormalizedValue(Pointer.START), 1);
+        this.normalValueEnd = isInteger ? roundAgainstSliderRange(clampAgainstStart) : clampAgainstStart;
     }
 
-    public float getRightValue()
+    public float getValue(Pointer pointer)
     {
-        return (float) MathUtils.denormalize(getRightNormalizedValue(), min, max);
+        return (float) MathUtils.denormalize(getNormalizedValue(pointer), min, max);
     }
 
-    public void setRightValue(float value)
+    public void setValue(float value, Pointer pointer)
     {
-        setRightNormalizedValue((float) MathUtils.clamp(MathUtils.normalize(value, min, max)));
+        setNormalizedValue((float) MathUtils.normalize(value, min, max), pointer);
     }
 
-    public float getRightNormalizedValue()
+    public float getNormalizedValue(Pointer pointer)
     {
-        return normalValueRight;
+        return Pointer.START.equals(pointer) ? getNormalizedValue() : normalValueEnd;
     }
 
-//    public float getLeftNormalizedValue()
-//    {
-//        return getNormalizedValue();
-//    }
-
-    public void setRightNormalizedValue(float normalValue)
+    public void setNormalizedValue(float normalValue, Pointer pointer)
     {
-        float value = clampBetweenAdjacents(false, normalValue);
-        // If an integer slider, round proportional to the max value
-        normalValueRight = isInteger ? (Math.round(value * max) / max) : value;
-        // handlePostCustomEvent();
-    }
-
-    @Override
-    public void setNormalizedValue(float normalValue)
-    {
-        super.setNormalizedValue(clampBetweenAdjacents(true, normalValue));
-        // handlePostCustomEvent();
-    }
-
-    @Override
-    public void onMouseDown(MouseDownEvent event)
-    {
-        super.onMouseDown(event);
-        isLastHeldPointerLeft = getClosestPointerToMouse(event.mouseX, event.mouseY);
-    }
-
-    @Override
-    public void onMouseScroll(MouseScrollEvent event)
-    {
-        isMovingTimer = 10;
-        float offset = (float) MathUtils.normalize(event.wheelDelta, min, max);
-
-        if (isLastHeldPointerLeft)
+        if (Pointer.START.equals(pointer))
         {
-            setNormalizedValue(getNormalizedValue() + offset);
+            // Prevent the start slider pointer from passing the end
+            float clampAgainstEnd = (float) MathUtils.clamp(normalValue, 0, getNormalizedValue(Pointer.END));
+            super.setNormalizedValue(clampAgainstEnd);
         }
         else
         {
-            setRightNormalizedValue(getRightNormalizedValue() + offset);
+            // Prevent the end slider pointer from passing the start
+            float clampAgainstStart = (float) MathUtils.clamp(normalValue, getNormalizedValue(Pointer.START), 1);
+            // If an integer slider, round proportional to the max value
+            normalValueEnd = isInteger ? roundAgainstSliderRange(clampAgainstStart) : clampAgainstStart;
+            tryApplyPostEventBehavior(MouseDownEvent.class);
         }
     }
 
-//    @Override
-//    public void onMouseDrag(int mouseX, int mouseY, int clickedMouseButton, long msHeld)
-//    {
-//        // TODO: move stuff from onScreenDraw here?
-//    }
+    @Override
+    protected float getPointerSizeRatio()
+    {
+        return 1.5F;
+    }
 
     @Override
     public void onScreenDraw(int mouseX, int mouseY, float partialTicks)
@@ -92,131 +83,175 @@ public class GuiDualSlider extends GuiSlider
         if (isMouseDown())
         {
             float value = getNormalValueAtScreenPos(mouseX, mouseY);
-
-            if (isLastHeldPointerLeft)
-            {
-                setNormalizedValue(value);
-            }
-            else
-            {
-                setRightNormalizedValue(value);
-            }
+            setNormalizedValue(value, lastHeldPointer);
         }
+        // The check against movingTimer lets onMouseScroll always 'claim' the closest pointer to mouse so that it shows
+        // as highlighted while scrolling without being immediately overridden by the standard isHovered() check. This
+        // extra check only matters if the last held pointer and the closest pointer are not equal during
+        // a MouseScroll, letting the highlight 'bounce' back and forth
+        else if (isHovered() && movingTimer == 0)
+        {
+            closestPointerToMouse = getClosestPointerToMouse(mouseX, mouseY);
+        }
+    }
+
+    @Override
+    public void onMouseDown(MouseDownEvent event)
+    {
+        super.onMouseDown(event);
+        lastHeldPointer = closestPointerToMouse;
+    }
+
+    @Override
+    public void onMouseScroll(MouseScrollEvent event)
+    {
+        movingTimer = 10;
+        setNormalizedValue(getNormalizedValue(lastHeldPointer) + scaleWheelDelta(event.wheelDelta), lastHeldPointer);
+        closestPointerToMouse = lastHeldPointer;
     }
 
     @Override
     protected void drawHorizontalSlider()
     {
-        float trackHeight = (getHeight() / 3F);
+        float trackHeight = getTrackThickness();
+        float trackY = worldYPos() + trackHeight;
 
-        float leftPointerScreenPos = getScreenPosAtNormalValue(getNormalizedValue());
-        float rightPointerScreenPos = getScreenPosAtNormalValue(getRightNormalizedValue());
+        float leftPointerScreenPos = getScreenPosAtNormalValue(getNormalizedValue(Pointer.START));
+        float rightPointerScreenPos = getScreenPosAtNormalValue(getNormalizedValue(Pointer.END));
 
-        // Left side track for all but first pointer
-//        float x = /*(i == 0) ?*/ xPos /*: getPointerScreenPos(i - 1)*/;
-//        float w = /*(i == 0) ? */ (leftPointerScreenPos - xPos) /*: (currPointerCenter - getPointerScreenPos(i - 1))*/;
-//        float pOffset = pointerSize / 2F;
+        // Start
+        RenderUtils.drawNineSliceTexturedRect(trackResource, worldXPos(), trackY, getZLevel(), (leftPointerScreenPos - worldXPos()), trackHeight,
+                32, 32, 5, 3, getColor(GuiColorType.ACCENT1));
+        // Middle
+        RenderUtils.drawNineSliceTexturedRect(trackResource, leftPointerScreenPos, trackY, getZLevel(), (rightPointerScreenPos - leftPointerScreenPos), trackHeight,
+                32, 32, 5, 3, getColor(GuiColorType.BASE));
+        // End
+        RenderUtils.drawNineSliceTexturedRect(trackResource, rightPointerScreenPos, trackY, getZLevel(), (getWidth() - (rightPointerScreenPos - worldXPos())), trackHeight,
+                32, 32, 5, 3, getColor(GuiColorType.ACCENT1));
 
-        // far left
-        RenderUtils.drawRectangle(worldXPos(), (worldYPos() + trackHeight), (leftPointerScreenPos - worldXPos()) /*- pOffset*/, trackHeight, getColor(GuiColorType.ACCENT1));
-
-        // middle
-        RenderUtils.drawRectangle(leftPointerScreenPos /*- pOffset*/, (worldYPos() + trackHeight), (rightPointerScreenPos - leftPointerScreenPos) /*+ (pOffset * 2)*/, trackHeight, getColor(GuiColorType.BASE));
-
-        // far right
-        RenderUtils.drawRectangle(rightPointerScreenPos /*+ pOffset*/, (worldYPos() + trackHeight), getWidth() - (rightPointerScreenPos - worldXPos()) /*- pOffset*/, trackHeight, getColor(GuiColorType.ACCENT1));
-
-        drawPointer(true);
-        drawPointer(false);
+        drawPointer(Pointer.START);
+        drawPointer(Pointer.END);
     }
 
-    // TODO:
+    @Override
     protected void drawVerticalSlider()
     {
-//        float trackWidth = (getWidth() / 3F);
-//
-//        // Draw track(s)
-//        for (int i = 0; i < normalPointerValues.length; i++)
-//        {
-//            float currPointerCenter = getPointerScreenPos(i);
-//
-//            // Right side track for last pointer
-//            if (i == normalPointerValues.length - 1)
-//            {
-//                float y = currPointerCenter;
-//                float h = height - (currPointerCenter - yPos);
-//
-//                RenderUtils.drawRoundedRect(GL11.GL_POLYGON, xPos + trackWidth, y, trackWidth, h, parent.getOuterRadius(), parent.getColorMap().get("ACCENT"), true);
-//            }
-//
-//            // Left side track for all but first pointer
-//            float y = (i == 0) ? yPos : getPointerScreenPos(i - 1);
-//            float h = (i == 0) ? (currPointerCenter - yPos) : (currPointerCenter - getPointerScreenPos(i - 1));
-//
-//            RenderUtils.drawRoundedRect(GL11.GL_POLYGON, xPos + trackWidth, y, trackWidth, h, parent.getOuterRadius(), colors.get(i), true);
-//        }
-//
-//        drawPointer(true);
-//        drawPointer(false);
+        float trackWidth = getTrackThickness();
+        float trackX = worldXPos() + trackWidth;
+
+        float topPointerPos = getScreenPosAtNormalValue(getNormalizedValue(Pointer.START));
+        float bottomPointerPos = getScreenPosAtNormalValue(getNormalizedValue(Pointer.END));
+
+        // Start
+        RenderUtils.drawNineSliceTexturedRect(trackResource, trackX, worldYPos(), getZLevel(), trackWidth, (topPointerPos - worldYPos()),
+                32, 32, 5, 3, getColor(GuiColorType.ACCENT1));
+        // Middle
+        RenderUtils.drawNineSliceTexturedRect(trackResource, trackX, topPointerPos, getZLevel(), trackWidth, (bottomPointerPos - topPointerPos),
+                32, 32, 5, 3, getColor(GuiColorType.BASE));
+        // End
+        RenderUtils.drawNineSliceTexturedRect(trackResource, trackX, bottomPointerPos, getZLevel(), trackWidth, (getHeight() - (bottomPointerPos - worldYPos())),
+                32, 32, 5, 3, getColor(GuiColorType.ACCENT1));
+
+        drawPointer(Pointer.START);
+        drawPointer(Pointer.END);
     }
 
-    protected void drawPointer(boolean isLeftPointer)
+    protected void drawPointer(Pointer pointer)
     {
-        float screenPos = getScreenPosAtNormalValue(isLeftPointer ? getNormalizedValue() : getRightNormalizedValue());
-        float x = isVertical ? worldXPos() : screenPos - (getPointerSize() / 2F);
-        float y = isVertical ? screenPos - (getPointerSize() / 2F) : worldYPos();
+        boolean isStartPointer = Pointer.START.equals(pointer);
+        boolean shouldScale = (movingTimer > 0 || hoveredTimer > 0) && closestPointerToMouse.equals(pointer);
 
-        if (isLeftPointer)
+        float size = getPointerSize();
+        float movingOffset = size / 4;
+
+        if (shouldScale)
         {
-            RenderUtils.drawTexturedRect(pointerResource, x, y, getZLevel(), getPointerSize(), getPointerSize(), getColor(GuiColorType.BASE));
+            size += movingOffset;
+        }
+
+        float trackPos = (isVertical ? worldXPos() : worldYPos()) + getTrackThickness();
+        float pointerPos = getScreenPosAtNormalValue(isStartPointer ? getNormalizedValue(Pointer.START) : getNormalizedValue(Pointer.END));
+
+        float x = isVertical ? trackPos - (Math.abs(getTrackThickness() - size) / 2) : pointerPos - (size / 2);
+        float y = isVertical ? pointerPos - (size / 2) : trackPos - (Math.abs(getTrackThickness() - size) / 2);
+
+        float rotateX = x + (getPointerSize() / 2);
+        float rotateY = y + (getPointerSize() / 2);
+
+        if (shouldScale)
+        {
+            rotateX += (movingOffset / 2);
+            rotateY += (movingOffset / 2);
+        }
+
+        if (isVertical)
+        {
+            GL11.glPushMatrix();
+            RenderUtils.rotateCurrentMatrixAroundObject(rotateX, rotateY, (isStartPointer ? 90 : -90), 0, 0, 1);
+            RenderUtils.drawTexturedRect(pointerResource, x, y, getZLevel(), size, size, getColor(GuiColorType.ACCENT1));
+            GL11.glPopMatrix();
         }
         else
         {
-            GL11.glPushMatrix();
-            RenderUtils.rotateCurrentMatrixAroundObject(x + (getPointerSize() / 2), y + (getPointerSize() / 2),
-                    180, 0, 0, 1);
-            RenderUtils.drawTexturedRect(pointerResource, x, y, getZLevel(), getPointerSize(), getPointerSize(), getColor(GuiColorType.BASE));
-            GL11.glPopMatrix();
+            if (isStartPointer)
+            {
+                RenderUtils.drawTexturedRect(pointerResource, x, y, getZLevel(), size, size, getColor(GuiColorType.ACCENT1));
+            }
+            else
+            {
+                GL11.glPushMatrix();
+                RenderUtils.rotateCurrentMatrixAroundObject(rotateX, rotateY, 180, 0, 0, 1);
+                RenderUtils.drawTexturedRect(pointerResource, x, y, getZLevel(), size, size, getColor(GuiColorType.ACCENT1));
+                GL11.glPopMatrix();
+            }
         }
-    }
 
-    // what prevents sliders from passing each other
-    protected float clampBetweenAdjacents(boolean isLeftPointer, double normalValue)
-    {
-        double minValue = !isLeftPointer ? (getNormalizedValue()) : 0;
-        double maxValue = isLeftPointer ? (getRightNormalizedValue()) : 1;
-
-        return (float) MathUtils.clamp(normalValue, minValue, maxValue);
-    }
-
-    // return true = isLeftPointer
-    protected boolean getClosestPointerToMouse(int mouseX, int mouseY)
-    {
-        double mousePosSliderVal = getNormalValueAtScreenPos(mouseX, mouseY);
-
-        double leftDistance = Math.abs(getNormalizedValue() - mousePosSliderVal);
-        double rightDistance = Math.abs(getRightNormalizedValue() - mousePosSliderVal);
-
-        if (leftDistance != rightDistance)
+        if (debug)
         {
-            return rightDistance > leftDistance;
+            RenderUtils.drawRectangle(GL11.GL_LINE_LOOP, x, y, size, size, isFocused() ? new Color("#00ff00") : new Color("#ff55ff"));
+        }
+    }
+
+    protected Pointer getClosestPointerToMouse(int mouseX, int mouseY)
+    {
+        double normalMousePos = getNormalValueAtScreenPos(mouseX, mouseY);
+
+        double distanceFromMouseToStartPointer = Math.abs(getNormalizedValue(Pointer.START) - normalMousePos);
+        double distanceFromMouseToEndPointer = Math.abs(getNormalizedValue(Pointer.END) - normalMousePos);
+
+        // If the pointers don't perfectly overlap:
+        // Return the pointer with the smaller distance to the mouse
+        if (distanceFromMouseToStartPointer != distanceFromMouseToEndPointer)
+        {
+            return distanceFromMouseToStartPointer < distanceFromMouseToEndPointer ? Pointer.START : Pointer.END;
         }
 
-        return mousePosSliderVal < getNormalizedValue();
+        // This extra calculation is necessary to resolve perfectly overlapping pointers.
+        // If the pointers are perfectly overlapping we can't figure out which is closer to the mouse by comparing them
+        // to each other, so we have to compare them to the mouse instead.
+        // Also, we can't just use this as the only condition either because in that case this would always return END
+        // whenever the mouse is to the right/bottom of the start pointer
+        return normalMousePos < getNormalizedValue(Pointer.START) ? Pointer.START : Pointer.END;
+    }
+
+    public enum Pointer
+    {
+        START,
+        END
     }
 
     public static class Builder extends GuiSlider.AbstractBuilder<Builder, GuiDualSlider>
     {
-        protected float startValueRight;
+        protected float endPointerValue;
 
         public Builder(String elementName)
         {
             super(elementName);
         }
 
-        public Builder startValueRight(float startValueRight)
+        public Builder endPointerValue(float endPointerValue)
         {
-            this.startValueRight = startValueRight;
+            this.endPointerValue = endPointerValue;
             return self();
         }
 
--- src/main/java/io/jonuuh/core/lib/gui/element/slider/GuiSlider.java
+++ src/main/java/io/jonuuh/core/lib/gui/element/slider/GuiSlider.java
@@ -1,6 +1,8 @@
 package io.jonuuh.core.lib.gui.element.slider;
 
 import io.jonuuh.core.lib.gui.element.GuiElement;
+import io.jonuuh.core.lib.gui.event.GuiEvent;
+import io.jonuuh.core.lib.gui.event.PostEventBehaviorHost;
 import io.jonuuh.core.lib.gui.event.input.MouseDownEvent;
 import io.jonuuh.core.lib.gui.event.input.MouseScrollEvent;
 import io.jonuuh.core.lib.gui.event.lifecycle.ScreenTickEvent;
@@ -8,20 +10,29 @@ import io.jonuuh.core.lib.gui.listener.input.MouseClickListener;
 import io.jonuuh.core.lib.gui.listener.input.MouseScrollListener;
 import io.jonuuh.core.lib.gui.listener.lifecycle.ScreenTickListener;
 import io.jonuuh.core.lib.util.MathUtils;
+import net.minecraft.util.ResourceLocation;
 
 import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
 
-abstract class GuiSlider extends GuiElement implements MouseClickListener, MouseScrollListener, ScreenTickListener
+abstract class GuiSlider extends GuiElement implements MouseClickListener, MouseScrollListener, ScreenTickListener, PostEventBehaviorHost
 {
+    protected static final ResourceLocation trackResource = new ResourceLocation("core:textures/slider.png");
+    private final Map<Class<? extends GuiEvent>, Consumer<GuiElement>> postBehaviors;
     /** The minimum value of this slider (not normalized; readable value) */
     protected final float min;
     /** The maximum value of this slider (not normalized; readable value) */
     protected final float max;
-    protected final boolean isVertical;
+    protected final boolean isVertical; // TODO: refactor all instances of isVertical to isHorizontal?
     protected final boolean isInteger;
     protected final DecimalFormat decimalFormat;
     protected float normalValue;
-    protected int isMovingTimer;
+    /** This slider will be treated as 'moving' for this many more screen ticks */
+    protected int movingTimer;
+    /** This slider will be treated as 'hovered' for this many more screen ticks */
+    protected int hoveredTimer;
 
     private boolean enabled;
     private boolean mouseDown;
@@ -35,7 +46,17 @@ abstract class GuiSlider extends GuiElement implements MouseClickListener, Mouse
         this.isInteger = builder.isInteger;
         this.decimalFormat = builder.decimalFormat;
         this.enabled = builder.enabled;
-        setValue(builder.startValue);
+
+        this.postBehaviors = new HashMap<>();
+        if (builder.valueChangeBehavior != null)
+        {
+            assignPostEventBehavior(MouseDownEvent.class, builder.valueChangeBehavior);
+        }
+
+        // This is the same as `setValue(builder.startValue);`, but calling setValue() from constructor
+        // is a bad idea as it also tries to apply the event behavior in setNormalizedValue()
+        float value = (float) MathUtils.clamp(MathUtils.normalize(builder.startValue, min, max), 0, 1);
+        this.normalValue = isInteger ? roundAgainstSliderRange(value) : value;
     }
 
     public float getValue()
@@ -45,7 +66,7 @@ abstract class GuiSlider extends GuiElement implements MouseClickListener, Mouse
 
     public void setValue(float value)
     {
-        setNormalizedValue((float) MathUtils.clamp(MathUtils.normalize(value, min, max)));
+        setNormalizedValue((float) MathUtils.normalize(value, min, max));
     }
 
     public int getValueInt()
@@ -60,10 +81,65 @@ abstract class GuiSlider extends GuiElement implements MouseClickListener, Mouse
 
     public void setNormalizedValue(float normalValue)
     {
-        double value = MathUtils.clamp(normalValue, 0, 1);
-        // If an integer slider, round proportional to the max value
-        this.normalValue = isInteger ? (Math.round(value * max) / max) : (float) value;
-//        handlePostCustomEvent();
+        float value = (float) MathUtils.clamp(normalValue, 0, 1);
+        // If an integer slider, round proportional to the range of the slider
+        this.normalValue = isInteger ? roundAgainstSliderRange(value) : value;
+        tryApplyPostEventBehavior(MouseDownEvent.class);
+    }
+
+    protected float getPointerSizeRatio()
+    {
+        return 2;
+    }
+
+    protected float getPointerSize()
+    {
+        return isVertical ? getWidth() / getPointerSizeRatio() : getHeight() / getPointerSizeRatio();
+    }
+
+    protected float getTrackThickness()
+    {
+        return isVertical ? (getWidth() / 3F) : (getHeight() / 3F);
+    }
+
+    protected float scaleWheelDelta(int wheelDelta)
+    {
+        // wheelDelta divided by the range of the slider if integer, else 1% of wheelDelta
+        return isInteger ? wheelDelta / (max - min) : wheelDelta * 0.01F;
+    }
+
+    /**
+     * Used for setting normalized values of integer sliders
+     *
+     * @param clampedNormalValue An already normalized, already clamped value
+     * @return The given value rounded proportional to the range of the slider
+     */
+    protected float roundAgainstSliderRange(float clampedNormalValue)
+    {
+        float sliderRange = max - min;
+        return Math.round(clampedNormalValue * sliderRange) / sliderRange;
+    }
+
+    /**
+     * Calculate normal value along the slider given some world x,y position
+     * <p>
+     * Used to calculate where on the slider the mouse was clicked
+     */
+    protected float getNormalValueAtScreenPos(int xPos, int yPos)
+    {
+        return isVertical
+                ? (yPos - worldYPos()) / getHeight()
+                : (xPos - worldXPos()) / getWidth();
+    }
+
+    /**
+     * Calculate world x or y screen position of some normal value along the slider
+     */
+    protected float getScreenPosAtNormalValue(float normalValue)
+    {
+        return isVertical
+                ? (worldYPos() + (normalValue * getHeight()))
+                : (worldXPos() + (normalValue * getWidth()));
     }
 
     @Override
@@ -90,9 +166,10 @@ abstract class GuiSlider extends GuiElement implements MouseClickListener, Mouse
         this.mouseDown = mouseDown;
     }
 
-    protected float getPointerSize()
+    @Override
+    public Map<Class<? extends GuiEvent>, Consumer<GuiElement>> getPostEventBehaviors()
     {
-        return isVertical ? getWidth() : getHeight();
+        return postBehaviors;
     }
 
     @Override
@@ -112,11 +189,6 @@ abstract class GuiSlider extends GuiElement implements MouseClickListener, Mouse
         {
             drawHorizontalSlider();
         }
-
-        if (isHovered() || isMouseDown())
-        {
-            isMovingTimer = 5;
-        }
     }
 
     protected abstract void drawVerticalSlider();
@@ -129,42 +201,41 @@ abstract class GuiSlider extends GuiElement implements MouseClickListener, Mouse
         MouseClickListener.super.onMouseDown(event);
     }
 
+    // TODO: GuiSliders are currently the only case of onScreenDraw being used for something other than actually drawing elements.
+    //  Would be great to use onMouseDrag around here for setting normal value rather than onScreenDraw, except mouse
+    //  drag events fire from game ticking loop (every ~50ms) rather than from rendering loop (~<fps>/1000ms?). This makes
+    //  the slider appear "laggy" if not an integer slider.
+    //  Have thought about jumping through some extra hoops to make smooth sliders using onMouseDrag workable
+    //  (lerping from last value updated by onMouseDrag?), but you'd still ultimately need to set the normal value from onScreenDraw,
+    //  so the underlying problem isn't fixed
+
     @Override
     public void onMouseScroll(MouseScrollEvent event)
     {
-        isMovingTimer = 10;
-        setNormalizedValue(getNormalizedValue() + (float) MathUtils.normalize(event.wheelDelta, min, max));
+        movingTimer = 10;
+        setNormalizedValue(getNormalizedValue() + scaleWheelDelta(event.wheelDelta));
     }
 
     @Override
     public void onScreenTick(ScreenTickEvent event)
     {
-        if (isMovingTimer > 0)
+        if (isHovered())
         {
-            isMovingTimer--;
+            hoveredTimer = 5;
         }
-    }
+        else if (hoveredTimer > 0)
+        {
+            hoveredTimer--;
+        }
 
-    /**
-     * Calculate normal value along the slider given some world x,y position
-     * <p>
-     * Used to calculate where on the slider the mouse was clicked
-     */
-    protected float getNormalValueAtScreenPos(int xPos, int yPos)
-    {
-        return isVertical
-                ? (yPos - worldYPos()) / getHeight()
-                : (xPos - worldXPos()) / getWidth();
-    }
-
-    /**
-     * Calculate world x or y screen position of some normal value along the slider
-     */
-    protected float getScreenPosAtNormalValue(float normalValue)
-    {
-        return isVertical
-                ? (worldYPos() + (normalValue * getHeight()))
-                : (worldXPos() + (normalValue * getWidth()));
+        if (isMouseDown())
+        {
+            movingTimer = 5;
+        }
+        else if (movingTimer > 0)
+        {
+            movingTimer--;
+        }
     }
 
     public static abstract class AbstractBuilder<T extends GuiSlider.AbstractBuilder<T, R>, R extends GuiSlider> extends GuiElement.AbstractBuilder<T, R>
@@ -176,6 +247,7 @@ abstract class GuiSlider extends GuiElement implements MouseClickListener, Mouse
         protected boolean isInteger = false;
         protected DecimalFormat decimalFormat = new DecimalFormat("#.##");
         protected boolean enabled = true;
+        protected Consumer<GuiElement> valueChangeBehavior = null;
 
         protected AbstractBuilder(String elementName)
         {
@@ -216,6 +288,12 @@ abstract class GuiSlider extends GuiElement implements MouseClickListener, Mouse
         public T enabled(boolean enabled)
         {
             this.enabled = enabled;
+            return self();
+        }
+
+        public T stateChangeBehavior(Consumer<GuiElement> valueChangeBehavior)
+        {
+            this.valueChangeBehavior = valueChangeBehavior;
             return self();
         }
     }
