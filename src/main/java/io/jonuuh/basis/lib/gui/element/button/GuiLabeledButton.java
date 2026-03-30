369de7cb35b0697e0dc3e02d944fe01fc566da9c
jonuuh <jhammond.123x@gmail.com>
2026-03-30 02:03:34 -0700

    Rewrite GuiLabeledButton to be as identical to GuiLabel as possible

--- src/main/java/io/jonuuh/basis/lib/gui/element/button/GuiLabeledButton.java
+++ src/main/java/io/jonuuh/basis/lib/gui/element/button/GuiLabeledButton.java
@@ -1,16 +1,25 @@
 package io.jonuuh.basis.lib.gui.element.button;
 
+import io.jonuuh.basis.lib.gui.element.ElementUtils;
 import io.jonuuh.basis.lib.gui.properties.GuiColorType;
 import io.jonuuh.basis.lib.util.RenderUtils;
+import net.minecraft.client.gui.FontRenderer;
+import org.lwjgl.opengl.GL11;
 
 public class GuiLabeledButton extends GuiButton
 {
-    private String label;
+    protected final FontRenderer fontRenderer;
+    protected String label;
+    protected float textScale;
+    protected boolean doShadow;
 
     public GuiLabeledButton(Builder builder)
     {
         super(builder);
+        this.fontRenderer = mc.fontRendererObj;
         this.label = builder.label;
+        this.textScale = builder.textScale;
+        this.doShadow = builder.doShadow;
     }
 
     public String getLabel()
@@ -21,6 +30,16 @@ public class GuiLabeledButton extends GuiButton
     public void setLabel(String label)
     {
         this.label = label;
+
+        // Resize width and height of the button given the new label
+        float hPad = getPadding().left() + getPadding().right();
+        float vPad = getPadding().top() + getPadding().bottom();
+
+        float strW = (fontRenderer.getStringWidth(label) - 1) * textScale;
+        float strH = (fontRenderer.FONT_HEIGHT - 1) * textScale;
+
+        this.setWidth(strW + hPad);
+        this.setHeight(strH + vPad);
     }
 
     @Override
@@ -32,17 +51,39 @@ public class GuiLabeledButton extends GuiButton
         }
         super.onScreenDraw(mouseX, mouseY, partialTicks);
 
-        RenderUtils.drawRoundedRectWithBorder(worldXPos(), worldYPos(), getWidth(), getHeight(), getCornerRadius(), 1, getColor(GuiColorType.BASE), getColor(GuiColorType.BORDER));
+        // Draw button background
+        RenderUtils.drawRoundedRectWithBorder(worldXPos(), worldYPos(), getWidth(), getHeight(),
+                getCornerRadius(), 1, getColor(GuiColorType.BASE), getColor(GuiColorType.BORDER));
 
-        String buttonText = RenderUtils.trimStringToWidthWithEllipsis(label, (int) getWidth());
+        // See comment in GuiLabel regarding more or less identical functionality
+        String trimmedText = RenderUtils.trimStringToWidthWithEllipsis(label, (int) (ElementUtils.getInnerWidth(this) * (1 / textScale)));
 
-        mc.fontRendererObj.drawString(buttonText, worldXPos() + (getWidth() / 2) - ((float) mc.fontRendererObj.getStringWidth(buttonText) / 2),
-                worldYPos() + (getHeight() / 2) - ((float) mc.fontRendererObj.FONT_HEIGHT / 2), getColor(GuiColorType.ACCENT1).toPackedARGB(), true);
+        if (textScale != 1F)
+        {
+            GL11.glPushMatrix();
+
+            // Scale matrix around top left corner of element's drawable area (inner bound)
+            RenderUtils.scaleCurrentMatrixAroundPoint(
+                    ElementUtils.getInnerLeftBound(this), ElementUtils.getInnerTopBound(this),
+                    textScale, textScale
+            );
+        }
+
+        fontRenderer.drawString(trimmedText,
+                ElementUtils.getInnerLeftBound(this), ElementUtils.getInnerTopBound(this),
+                getColor(GuiColorType.ACCENT1).toPackedARGB(), doShadow);
+
+        if (textScale != 1F)
+        {
+            GL11.glPopMatrix();
+        }
     }
 
     public static class Builder extends GuiButton.AbstractBuilder<Builder, GuiLabeledButton>
     {
         protected String label = "";
+        protected float textScale = 1F;
+        protected boolean doShadow = true;
 
         public Builder(String elementName)
         {
@@ -55,6 +96,18 @@ public class GuiLabeledButton extends GuiButton
             return self();
         }
 
+        public Builder textScale(float textScale)
+        {
+            this.textScale = textScale;
+            return self();
+        }
+
+        public Builder doShadow(boolean doShadow)
+        {
+            this.doShadow = doShadow;
+            return self();
+        }
+
         @Override
         protected Builder self()
         {
@@ -64,6 +117,15 @@ public class GuiLabeledButton extends GuiButton
         @Override
         public GuiLabeledButton build()
         {
+            float hPad = padding.left() + padding.right();
+            float vPad = padding.top() + padding.bottom();
+
+            float strW = (mc.fontRendererObj.getStringWidth(label) - 1) * textScale;
+            float strH = (mc.fontRendererObj.FONT_HEIGHT - 1) * textScale;
+
+            this.width = strW + hPad;
+            this.height = strH + vPad;
+
             return new GuiLabeledButton(this);
         }
     }
