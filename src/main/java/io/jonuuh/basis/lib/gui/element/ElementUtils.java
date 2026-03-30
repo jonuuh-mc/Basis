5277cec61eaa951fcc9d4657db421a1e23b04620
jonuuh <jhammond.123x@gmail.com>
2026-03-29 23:52:45 -0700

    Clean up ElementUtils

--- src/main/java/io/jonuuh/basis/lib/gui/element/ElementUtils.java
+++ src/main/java/io/jonuuh/basis/lib/gui/element/ElementUtils.java
@@ -1,6 +1,47 @@
 package io.jonuuh.basis.lib.gui.element;
 
 // TODO: likely should move all bounding calculations into base GuiElement, we'll see
+
+/**
+ * <pre>
+ * {@code
+ *
+ *                       Left Pad                                         Right Pad
+ *                       ◀───────▶                                        ◀───────▶
+ *
+ *                     World    Inner                                   Inner
+ *                     X Pos    Left                                    Right
+ *                       │       │                                        │
+ *
+ *                       │       │                                        │
+ *        ▲   World ─ ─ ─╔════════════════════════════════════════════════════════╗─ ─ ─         ▲
+ *        │   Y Pos      ║▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒║              │
+ *    Top │              ║▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒║              │
+ *    Pad │              ║▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒║              │
+ *        │              ║▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒║              │
+ *        ▼   Inner ─ ─ ─║▒▒▒▒▒▒▒╔════════════════════════════════════════╗▒▒▒▒▒▒▒║─ ─ ─    ▲  Height
+ *            Top        ║▒▒▒▒▒▒▒║████████████████████████████████████████║▒▒▒▒▒▒▒║         │    │
+ *                       ║▒▒▒▒▒▒▒║████████████████████████████████████████║▒▒▒▒▒▒▒║         │    │
+ *                       ║▒▒▒▒▒▒▒║████████████████████████████████████████║▒▒▒▒▒▒▒║       Inner  │
+ *                       ║▒▒▒▒▒▒▒║████████████████████████████████████████║▒▒▒▒▒▒▒║      Height  │
+ *                       ║▒▒▒▒▒▒▒║████████████████████████████████████████║▒▒▒▒▒▒▒║         │    │
+ *                       ║▒▒▒▒▒▒▒║████████████████████████████████████████║▒▒▒▒▒▒▒║         │    │
+ *        ▲   Inner ─ ─ ─║▒▒▒▒▒▒▒╚════════════════════════════════════════╝▒▒▒▒▒▒▒║─ ─ ─    ▼    │
+ *        │   Bottom     ║▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒║              │
+ * Bottom │              ║▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒║              │
+ *    Pad │              ║▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒║              │
+ *        │              ║▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒║              │
+ *        ▼              ╞═══════╤════════════════════════════════════════╤═══════╝              ▼
+ *
+ *                       │       │                                        │
+ *
+ *                               ◀───────────────Inner─Width──────────────▶
+ *
+ *                       ◀──────────────────────────Width─────────────────────────▶
+ *
+ * }
+ * </pre>
+ */
 public final class ElementUtils
 {
     public static float getInnerLeftBound(GuiElement element)
@@ -33,38 +74,33 @@ public final class ElementUtils
         return getInnerBottomBound(element) - getInnerTopBound(element);
     }
 
-//    public static void drawTexturedRect(GuiElement element, ResourceLocation texture, Color color)
+//    public static float getOuterLeftBound(GuiElement element)
 //    {
-//        RenderUtils.drawTexturedRect(texture, getInnerLeftBound(element), getInnerTopBound(element), element.getZLevel(), getInnerWidth(element), getInnerHeight(element), color);
+//        return element.worldXPos() - element.getMargin().left();
 //    }
-
-    public static float getOuterLeftBound(GuiElement element)
-    {
-        return element.worldXPos() - element.getMargin().left();
-    }
-
-    public static float getOuterRightBound(GuiElement element)
-    {
-        return element.worldXPos() + element.getWidth() + element.getMargin().right();
-    }
-
-    public static float getOuterTopBound(GuiElement element)
-    {
-        return element.worldYPos() - element.getMargin().top();
-    }
-
-    public static float getOuterBottomBound(GuiElement element)
-    {
-        return element.worldYPos() + element.getHeight() + element.getMargin().bottom();
-    }
-
-    public static float getOuterWidth(GuiElement element)
-    {
-        return getOuterRightBound(element) - getOuterLeftBound(element);
-    }
-
-    public static float getOuterHeight(GuiElement element)
-    {
-        return getOuterBottomBound(element) - getOuterTopBound(element);
-    }
+//
+//    public static float getOuterRightBound(GuiElement element)
+//    {
+//        return element.worldXPos() + element.getWidth() + element.getMargin().right();
+//    }
+//
+//    public static float getOuterTopBound(GuiElement element)
+//    {
+//        return element.worldYPos() - element.getMargin().top();
+//    }
+//
+//    public static float getOuterBottomBound(GuiElement element)
+//    {
+//        return element.worldYPos() + element.getHeight() + element.getMargin().bottom();
+//    }
+//
+//    public static float getOuterWidth(GuiElement element)
+//    {
+//        return getOuterRightBound(element) - getOuterLeftBound(element);
+//    }
+//
+//    public static float getOuterHeight(GuiElement element)
+//    {
+//        return getOuterBottomBound(element) - getOuterTopBound(element);
+//    }
 }
