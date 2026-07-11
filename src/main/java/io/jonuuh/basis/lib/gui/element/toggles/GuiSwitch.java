1e1ba793ab771454ac3b43b5afeead636627f3c7
jonuuh <jhammond.123x@gmail.com>
2026-07-11 04:21:52 -0700

    Add color functs to GuiSwitch Builder

--- src/main/java/io/jonuuh/basis/lib/gui/element/toggles/GuiSwitch.java
+++ src/main/java/io/jonuuh/basis/lib/gui/element/toggles/GuiSwitch.java
@@ -1,15 +1,22 @@
 package io.jonuuh.basis.lib.gui.element.toggles;
 
-import io.jonuuh.basis.lib.gui.properties.GuiColorType;
 import io.jonuuh.basis.lib.util.Color;
 import io.jonuuh.basis.lib.util.RenderUtils;
 
 public class GuiSwitch extends GuiToggle
 {
+    protected Color pointerColor;
+    protected Color enabledTrackColor;
+    protected Color disabledTrackColor;
+
     // TODO: make vertical option
     public GuiSwitch(Builder builder)
     {
         super(builder);
+
+        this.pointerColor = builder.pointerColor;
+        this.enabledTrackColor = builder.enabledTrackColor;
+        this.disabledTrackColor = builder.disabledTrackColor;
     }
 
     public float getPointerSize()
@@ -33,22 +40,27 @@ public class GuiSwitch extends GuiToggle
                     getCornerRadius(), 1, getBackgroundColor(), getBorderColor());
         }
 
-        float padding = ((getHeight() - getPointerSize()) / 2F);
+        float padding = (getHeight() - getPointerSize()) / 2F;
         float pointerX = isToggled() ? (worldXPos() + getWidth() - getPointerSize() - padding) : worldXPos() + padding;
-        Color trackColor = isToggled() ? getColor(GuiColorType.BASE) : getColor(GuiColorType.ACCENT2);
+        Color trackColor = isToggled() ? enabledTrackColor : disabledTrackColor;
 
         // Track
         RenderUtils.drawRoundedRectWithBorder(worldXPos(), worldYPos(),
                 getWidth(), getHeight(),
                 getCornerRadius(), 1, trackColor, getBorderColor());
+
         // Pointer
         RenderUtils.drawRoundedRectWithBorder(pointerX, worldYPos() + padding,
                 getPointerSize(), getPointerSize(),
-                getCornerRadius(), 1, getColor(GuiColorType.ACCENT1), getBorderColor());
+                getCornerRadius(), 1, pointerColor, getBorderColor());
     }
 
     public static class Builder extends GuiToggle.AbstractBuilder<Builder, GuiSwitch>
     {
+        protected Color pointerColor = Color.WHITE;
+        protected Color enabledTrackColor = Color.DARK_GREEN;
+        protected Color disabledTrackColor = Color.DARK_GRAY;
+
         public Builder(String elementName)
         {
             super(elementName);
@@ -56,6 +68,24 @@ public class GuiSwitch extends GuiToggle
             this.height = DEFAULT_HEIGHT;
             // Override GuiElement's default of drawing the background
             drawBackground(false);
+        }
+
+        public Builder pointerColor(Color color)
+        {
+            this.pointerColor = color;
+            return self();
+        }
+
+        public Builder enabledTrackColor(Color color)
+        {
+            this.enabledTrackColor = color;
+            return self();
+        }
+
+        public Builder disabledTrackColor(Color color)
+        {
+            this.disabledTrackColor = color;
+            return self();
         }
 
         @Override
